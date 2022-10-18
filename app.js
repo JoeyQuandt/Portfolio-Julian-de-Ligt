8efed65a4283ba29c779721c5997939d5b9f48ed
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2022-10-18 15:49:46 +0200

    Hamburger menu
    hamburger menu toegevoegd voor responsive menu

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ window.addEventListener("scroll", function(){
 
     header.classList.toggle("scrolling-active",windowPosition)
 })
+
+/*Hamburger Section*/
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
+  });
+});
