369b396e69d39a4a46b266c22c9af497e501d8e5
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2022-10-18 15:22:44 +0200

    Begin home page
    Kleuren,typografie begin css gemaakt en begonnen aan de navigatie balk en de home page

---
+++ app.js
@@ -0,0 +1,7 @@
+/*Color change after scrolling*/
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    let windowPosition = window.scrollY>0;
+
+    header.classList.toggle("scrolling-active",windowPosition)
+})
