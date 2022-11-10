6c20a205b5428ededebf310758305c5fd33f4441
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2022-11-10 11:48:43 +0100

    verbeteringen

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-/*Color change after scrolling*/
-window.addEventListener("scroll", function(){
-    let header = document.querySelector("header");
-    let windowPosition = window.scrollY>0;
-
-    header.classList.toggle("scrolling-active",windowPosition)
-})
-
 /*Hamburger Section*/
-const nav = document.querySelector(".nav-links");
+const nav = document.querySelector(".navbar-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
 
